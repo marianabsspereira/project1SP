21632ac8955541b889e4e5e125d1fd56c4fcf498
Mariana Pereira <marianabsspereira@gmail.com>
2021-08-02 17:56:09 +0100

    Project created

---
+++ src/main/java/com/educandoweb/project1SP/Project1SpApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.project1SP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1SpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1SpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/project1SP/Project1SpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.project1SP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1SpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
